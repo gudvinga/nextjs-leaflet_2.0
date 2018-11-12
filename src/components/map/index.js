aa4bb962e87e9bab6b6f4b5e2205e9da1239cc86
Aliaksei Hudzimchyk <31861355+gudvinga@users.noreply.github.com>
2018-11-12 20:07:29 +0300

    Installed markercluster (#1)

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 const position = [53.8999964, 27.5666644];
 
 export default () => (
   <div>
-    <Map center={position} zoom={13} style={{ height: 500 }}>
+    <Map center={position} zoom={13} maxZoom={18} style={{ height: 500 }}>
       <TileLayer
         url="http://tiles.maps.sputnik.ru/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -17,6 +18,12 @@ export default () => (
           Easily customizable.
         </Popup>
       </Marker>
+      <MarkerClusterGroup>
+        <Marker position={[53.910, 27.610]} />
+        <Marker position={[53.911, 27.611]} />
+        <Marker position={[53.912, 27.612]} />
+        <Marker position={[53.913, 27.613]} />
+      </MarkerClusterGroup>
     </Map>
   </div>
 );
